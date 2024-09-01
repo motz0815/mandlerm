67faa843a08bab06ae32f1e3a1310d2edb53d81f
Matthias <me@motz.xyz>
2024-09-01 21:50:44 +0200

    email sending

--- app/actions.ts
+++ app/actions.ts
@@ -1,4 +1,5 @@
 "use server"
+import { Resend } from "resend"
 
 export async function sendMessage(values: {
     name: string
@@ -8,13 +9,27 @@ export async function sendMessage(values: {
     error: string | null
     ok: boolean
 }> {
-    // simulate server action
-    await new Promise((resolve) => setTimeout(resolve, 1000))
+    const resend = new Resend(process.env.RESEND_API_KEY)
 
-    if (values.name === "Matthias") {
+    const { error } = await resend.emails.send({
+        from: "noreply@mandlerm.com",
+        to: "me@motz.xyz",
+        subject: "New message from your website from " + values.name,
+        replyTo: values.email,
+        html:
+            "<p>Hi Matthias, you have a new message from your website:</p><p><strong>Name:</strong> " +
+            values.name +
+            "</p><p><strong>Email:</strong> " +
+            values.email +
+            "</p><p><strong>Message:</strong> " +
+            values.message +
+            "</p>",
+    })
+
+    if (error) {
         return {
             ok: false,
-            error: "Name already taken.",
+            error: error.message,
         }
     }
 
